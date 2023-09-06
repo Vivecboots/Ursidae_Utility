c25fd10691109213f75dfd0b0dca7c61d2375834
Vivecboots <Blairwin05@gmail.com>
2023-09-06 12:57:34 -0700

    9/6

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@oasisprotocol/sapphire-hardhat";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  solidity: "0.8.7",
+  solidity: "0.8.9",
+};
+
+export default config;
+
+
+
+
+
